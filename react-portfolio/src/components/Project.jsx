5e0a2d0914b2988905ee3deebfdb42e893f397c1
Nattan Aguia <nattan23@msn.com>
2024-12-13 21:22:31 -0500

    project folder and portfolio folder

--- react-portfolio/src/components/Project.jsx
+++ react-portfolio/src/components/Project.jsx
@@ -1,11 +1,48 @@
 function Project () {
-    const { name, repo, link, description } = props;
+
 
     return (
-        <div
-    )
+        <div>
+        <h2>Portfolio</h2>
+        <section class="page-section">
+         
+         <h2>Work</h2>
+         <div class="flex-container">
+          
+             <div class="flex-item brain-games">
+              
+              <div class="box-title">
+  
+              <h3>Brain Games</h3>
+              <h5>JavaScript/HTML/CSS</h5>
+            </div>
+  
+             </div>
+  
+             <div class="flex-item readme-generator">
+              <div class="box-title">
+  
+              <h3>README-Generator</h3>
+              <h5>JavaScript/Node</h5>
+              </div>
+  
+             </div>
+  
+             <div class="flex-item vehicle-builder">
+              <div class="box-title">
+  
+              <h3>Vehicle Builder</h3>
+              <h5>TypeScript/Node</h5>
+              </div>
+  
+             </div>
+  
+         </div>
+      </section>
+      </div>
+    );
+  }
+    
 // Return the proyect componet for each individual proyect props name repo link description.
-
-}
 
 export default Project
--- react-portfolio/src/pages/Portfolio.jsx
+++ react-portfolio/src/pages/Portfolio.jsx
@@ -1,63 +1,9 @@
+import Project from "../components/Project";
+
 function Portfolio() {
+
   return (
-    <div>
-      <h2>Portfolio</h2>
-      <section class="page-section">
-       
-       <h2>Work</h2>
-       <div class="flex-container">
-        
-           <div class="flex-item surf-report">
-            
-            <div class="box-title">
-
-            <h3>Surf Report</h3>
-            <h5>MERN Stack</h5>
-          </div>
-
-           </div>
-
-           <div class="flex-item led-wall">
-            <div class="box-title">
-
-            <h3>LED Wall</h3>
-            <h5>Node/loT</h5>
-            </div>
-
-           </div>
-
-           <div class="flex-item react-calc">
-            <div class="box-title">
-
-            <h3>Calculator</h3>
-            <h5>React/JavaScript/CSS</h5>
-            </div>
-
-           </div>
-
-           <div class="flex-item pastel-puzzles">
-            <div class="box-title">
-
-            <h3>Pastel Puzzles</h3>
-            <h5>MERN Stack</h5>
-            </div>
-
-           </div>
-
-           <div class="flex-item run-buddy">
-            <div class="box-title">
-        
-
-            <h3>Run Buddy</h3>
-            <h5>HTML/CSS</h5>
-            </div>
-
-           </div>
-
-
-       </div>
-    </section>
-    </div>
+    <Project />
   );
 }
 
